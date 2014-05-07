4c72805221305ca05e31e0875e4be1bef4641371
Panzerwaffe <ulllan@yandex.ru>
2014-05-07 18:29:34 +0400

    dghd

---
+++ public/js/fivephoto.js
@@ -0,0 +1,6 @@
+$(function(){
+$('.fivephoto img:gt(0)').hide();
+setInterval(function()
+{$('.fivephoto :first-child').fadeOut(1000).next('img').fadeIn(1000)
+.end().appendTo('.fivephoto');}, 2000);
+});

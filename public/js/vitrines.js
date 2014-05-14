aa7cbb7a4cfbe5f3da49c8cfd5f378ca7aad3d24
Panzerwaffe <ulllan@yandex.ru>
2014-05-14 16:45:02 +0400

    sgrsdghth

--- public/js/vitrines.js
+++ public/js/vitrines.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-			$(".fancybox-effects-c").fancybox({
-				wrapCSS    : 'fancybox-custom',
-				closeClick : true,
-				openEffect : 'none',
-				helpers : {
-					overlay : {
-						css : {
-							'background' : 'rgba(238,238,238,0.85)' 
-						}
-					}
-				}
-			});
-		});
+	$(".fancybox-effects-c").fancybox({
+		wrapCSS    : 'fancybox-custom',
+		closeClick : true,
+		openEffect : 'none',
+		helpers : {
+			overlay : {
+				css : {'background' : 'rgba(238,238,238,0.85)'}
+			}
+		}
+	});
+});
